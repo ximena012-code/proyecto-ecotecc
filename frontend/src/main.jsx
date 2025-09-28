c05102f598302ab6176a57f0d60ce7287346f8cb
ximena012-code <ximenasarrazolabustamante@gmail.com>
2025-09-28 15:13:36 -0500

    Fix pago-exitoso routing and add SPA redirects

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -74,8 +74,7 @@ createRoot(document.getElementById('root')).render(
                 <Route path="historial-facturas" element={<UserRoute><HistorialFacturas /></UserRoute>} />
                 <Route path="pagar" element={<UserRoute><PaginaPago /></UserRoute>} />
                 <Route path="detalle-pedido" element={<UserRoute><DetallePedido /></UserRoute>} />
-                <Route path="pago-exitoso" element={<PagoExitoso />}/>
-
+<Route path="pago-exitoso" element={<UserRoute><PagoExitoso /></UserRoute>}/>
                 {/* Rutas de administrador */}
                 <Route path="productos" element={<AdminRoute><Productos/></AdminRoute>} />
                 <Route path="dashboardadmi" element={<AdminRoute><Dashboardadmi/></AdminRoute>} />
