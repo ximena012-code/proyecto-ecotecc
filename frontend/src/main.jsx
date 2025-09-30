894373326a3ddb6fa7665292c52287106d21f9e6
ximena012-code <ximenasarrazolabustamante@gmail.com>
2025-09-30 06:52:10 -0500

    Fix backend URL for product images

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -61,7 +61,7 @@ createRoot(document.getElementById('root')).render(
                 <Route path="estado-reparacion" element={<EstadoAdminReparacion />} />
                 <Route path="productos/:slug" element={<CategoriaPage />} />
                 <Route path="/producto/:id" element={<DetalleProducto />} /> 
-                <Route path="carrito" element={<UserRoute><Carrito /></UserRoute>} />
+                <Route path="carrito" element={<Carrito />} />
                 <Route path="pedido" element={<Pedido />} />
                 
                 {/* Notificaciones - Accesible para usuarios autenticados */}
